74694237e9bef4fbad7fd487e4a56bc9027a74d7
tngusmiso <k1miso012@gmail.com>
2019-01-23 11:09:02 +0900

    [python] 기본연산자

---
+++ Day3/1_python/003_기본연산자.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+
+a = 20
+b = 3
+
+print( a + b )
+print( a - b )  
+print( a / b )  # 정수
+print( a // b ) # 실수
+print( a * b )  # 곱셈
+print( a % b )  # 나머지
+print( a ** b ) # 제곱
