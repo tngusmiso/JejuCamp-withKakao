86620087792c8063c8e93e112e5a5a63e6add136
tngusmiso <k1miso012@gmail.com>
2019-01-23 11:09:43 +0900

    [python] 비교연산

---
+++ Day3/1_python/005_비교연산.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+
+a = 10
+b = 3
+print(a == b)   # False
+print(a > b)    # True
+print(a >= b)
+print(a < b)
+print(a <= b)
+print(a != b)
+
+c = '10'
+d = 10
+print( c == d ) # False     # 타입까지 비교함
