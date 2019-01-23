6af89caf3bc9ea17feeb08282c1e94be779e6d7f
tngusmiso <k1miso012@gmail.com>
2019-01-23 15:39:39 +0900

    [python] 조건문

---
+++ Day3/1_python/018_if.py
@@ -0,0 +1,9 @@
+#-*- coding: utf-8 -*-
+
+if True:
+    print('hello Jeju')
+
+if False:
+    print('hello Suhyun')
+
+print('Good Job')
---
+++ Day3/1_python/019_elif_else.py
@@ -0,0 +1,22 @@
+#-*- coding: utf-8 -*-
+
+score = 95
+money = 1000
+
+if score > 90:
+    print('mom : I\'m so happy')
+    money += 1000000
+elif score > 80:
+    print('mom : I\'m so happy')
+    money += 100000
+else:
+    print('mom : I\'m so happy')
+    money += 10000
+
+print(money)
+
+# 이스케이프 문자 (\n, \t, \', \", \\)
+print("hello world", end=" ")
+print("one", end=" ")
+print("two", end=" ")
+print("three", end=" ")
