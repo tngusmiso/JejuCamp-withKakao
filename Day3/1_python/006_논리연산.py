d6f750072a8feb386a53b2f0d9e22d80de9838d3
tngusmiso <k1miso012@gmail.com>
2019-01-23 11:13:53 +0900

    [python] 논리연산 예제

--- Day3/1_python/006_논리연산.py
+++ Day3/1_python/006_논리연산.py
@@ -6,3 +6,13 @@ b = False   # 0
 print(a and b)  # and는 *
 print(a or b)   # or는 +
 print(not a)
+
+print(bool(1))
+print(bool('hello'))
+print(bool(' '))
+print(bool(''))
+print(bool(0))
+
+for i in range(100):    # 0부터 99까지의 숫자 중에
+    if i % 3 == 0  or i % 5 == 0:   # 3과 5의 배수를 출력
+        print(i)
